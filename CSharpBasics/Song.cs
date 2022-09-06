e27933356106b25cfc99082acc8955235511fe3c
Thomas Backers <thomasbackers@gmail.com>
2022-09-06 10:32:18 +0200

    Ajoutez des fichiers projet.

---
+++ CSharpBasics/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasics
+{
+    internal class Song
+    {
+        public string title;
+        public string artist;
+        public int duration;
+        public static int songCount = 0;
+
+        public Song(string aTitle, string anArtist, int aDuration)
+        {
+            this.title = aTitle;
+            this.artist = anArtist;
+            this.duration = aDuration;
+            songCount++;
+        }
+    }
+}
